681b17ab233f38fb5fe2b5b2fa46145c47d6a748
Matteo Beccati <matteo@beccati.com>
2016-02-16 12:33:09 +0100

    Media partners helper for one page conf sites

---
+++ helper/onepage.js
@@ -0,0 +1,22 @@
+(function (data) {
+    var $mp = $("#media-partners");
+    var tag = $mp.data("tag");
+
+    $.each(data, function(k, v) {
+        if (v.tags && (!tag || $.inArray(tag, v.tags) == -1)) {
+            return;
+        }
+
+        $mp.append($("<A>")
+            .attr("href", v.url)
+            .attr("title", v.name)
+            .addClass("banner")
+            .append($("<IMG>")
+                .attr("src", v.img)
+                .attr("title", v.name)
+            )
+        );
+    });
+
+    $mp.append($("<DIV>").addClass("clear"));
+})({{ site.data.media_partners | sort: 'name' | jsonify }});
