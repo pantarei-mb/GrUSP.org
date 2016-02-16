5106b84dea783be53a72503ee09ccd5b1cd58ae2
Matteo Beccati <matteo@beccati.com>
2016-02-16 18:36:50 +0100

    Media partners for the main conferences

--- helpers/2016/main.js
+++ helpers/2016/main.js
@@ -1,8 +1,8 @@
 ---
 ---
 (function (data) {
-    var $mp = $("#media_partner");
-    var tags = [ "main" ];
+    var $mp = $("#media-partners");
+    var tags = $mp.data("tags") ?  $mp.data("tags").split(/ +/) : [];
 
     $.each(data, function(k, v) {
         if (v.tag && -1 == $.inArray(v.tag, tags)) {
@@ -13,6 +13,7 @@
             .attr("href", v.url)
             .attr("title", v.name)
             .addClass("banner")
+            .append($("<span class=\"ribbon media\">media</span>"))
             .append($("<IMG>")
                 .attr("src", "{{ site.baseurl }}/img/media-partners/" + v.img)
                 .attr("alt", v.name)
