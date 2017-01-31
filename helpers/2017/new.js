ed370a20daf94f5c9e1781ba8532b487827889c2
Matteo Beccati <matteo@beccati.com>
2017-01-31 14:16:50 +0100

    Helper for the new design

---
+++ helpers/2017/new.js
@@ -0,0 +1,37 @@
+---
+---
+(function (d, data) {
+    var mp = d.getElementsByClassName('sponsors__partner')[0];
+    var tags = [ "main" ];
+
+    data.forEach(function(v, k) {
+        var hd, ha, hi;
+
+        if (v.tags) {
+            var c = v.tags.filter(function(n) {
+                return tags.indexOf(n) != -1;
+            });
+
+            if (!c.length) {
+                return;
+            }
+        }
+
+        hi = d.createElement("DIV");
+        hi.setAttribute("style", "background-image: url({{ site.baseurl }}/img/media-partners/" + v.img + ')');
+        hi.setAttribute("class", "sponsors__image");
+
+        ha = d.createElement("A");
+        ha.setAttribute("href", v.url);
+        ha.setAttribute("title", v.name);
+        ha.setAttribute("class", "sponsors__link");
+        ha.appendChild(hi);
+
+        hd = d.createElement("DIV");
+        hd.setAttribute("class", "sponsors__partner-column");
+        hd.appendChild(ha);
+
+        mp.appendChild(hd);
+    });
+
+})(document, {{ site.data.media_partners_2017 | sort: 'name' | jsonify }});
